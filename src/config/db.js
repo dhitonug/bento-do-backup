5f3318894833a6e75f327dc79c4226c4f9232e8d
Dhito Nugroho <dhito0987@gmail.com>
2026-05-06 19:45:25 +0700

    feat: konfigurasi server express dan koneksi database neon.tech

--- src/config/db.js
+++ src/config/db.js
@@ -4,7 +4,20 @@ import dotenv from "dotenv";
 dotenv.config();
 const { Pool } = pkg;
 
-// membuat koneksi ke portgresql dengan pooling
+// membuat koneksi ke postgresql dengan pooling
 export const db = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+// Tes koneksi ke Neon.tech
+db.connect((err, client, release) => {
+  if (err) {
+    console.error("❌ Gagal menyambung ke Neon.tech:", err.message);
+  } else {
+    console.log("✅ Berhasil tersambung ke Database Neon.tech!");
+  }
+  if (client) release();
 });
--- src/server.js
+++ src/server.js
@@ -1,12 +1,22 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+// Memanggil koneksi database 
+import { db } from "./config/db.js";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// === MIDDLEWARES GLOBAL ===
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello World! (dan selamat datang di API Bento-do)");
 });
 
 app.listen(PORT, () => {
